769ccd8e80b6ddcfd71b16f6126f9ce344e71826
hemera <hemerocallisongml@gmail.com>
2020-04-19 13:11:07 +0700

    rerun of failed tests example via plagin

---
+++ section3/section3_lesson6_step7_rerun_test.py
@@ -0,0 +1,10 @@
+link = "http://selenium1py.pythonanywhere.com/"
+
+# pytest -v --tb=line --reruns 1 --browser_name=chrome section3_lesson6_step7_rerun_test.py
+def test_guest_should_see_login_link_pass(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#login_link")
+
+def test_guest_should_see_login_link_fail(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector("#magic_link")
